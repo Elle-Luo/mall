d98e046036553ca2ea3041b8507a31ecec4bca75
luohaoyue <1711370183@qq.com>
2021-05-23 23:26:39 +0800

    提交目录结构划分

---
+++ src/components/common/tabbar/tabbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="tabbar">
+    <slot></slot>
+  </div>
+</template>
+  
+<script>
+export default {
+  name:"tabBar"
+};
+</script>
+<style scoped>
+#tabbar {
+  width: 100%;
+  display: flex;
+  background-color: #f6f6f6;
+  position: fixed;
+  bottom: 0;
+  box-shadow: 0px -1px 1px rgba(100, 100, 100, 0.1);
+}
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,77 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="blue">
+      <img
+        slot="item-icon"
+        src="@/assets/img/tabbar/home.svg"
+        alt=""
+        class="img"
+      />
+      <img
+        slot="item-icon-active"
+        src="@/assets/img/tabbar/home_active.svg"
+        alt=""
+        class="img"
+      />
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="pink">
+      <img
+        slot="item-icon"
+        src="@/assets/img/tabbar/category.svg"
+        alt=""
+        class="img"
+      />
+      <img
+        slot="item-icon-active"
+        src="@/assets/img/tabbar/category_active.svg"
+        alt=""
+        class="img"
+      />
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart" activeColor="orange">
+      <img
+        slot="item-icon"
+        src="@/assets/img/tabbar/shopcart.svg"
+        alt=""
+        class="img"
+      />
+      <img
+        slot="item-icon-active"
+        src="@/assets/img/tabbar/shopcart_active.svg"
+        alt=""
+        class="img"
+      />
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img
+        slot="item-icon"
+        src="@/assets/img/tabbar/profile.svg"
+        alt=""
+        class="img"
+      />
+      <img
+        slot="item-icon-active"
+        src="@/assets/img/tabbar/profile_active.svg"
+        alt=""
+        class="img"
+      />
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import tabBar from "@/components/tabbar/tabbar";
+import TabBarItem from "@/components/tabbar/tabBarItem.vue";
+export default {
+  components: {
+    tabBar,
+    TabBarItem,
+  },
+};
+</script>
+<style scoped>
+</style>
