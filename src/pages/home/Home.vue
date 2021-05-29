72e0232cc209b0faf54aa8b8b0e7633a9c27a8e2
luohaoyue <1711370183@qq.com>
2021-05-29 15:11:53 +0800

    导航栏开发

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home__nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
-  export default {
-
-  }
+import NavBar from "components/common/navbar/NavBar";
+export default {
+  components: {
+    NavBar,
+  },
+};
 </script>
 <style scoped>
-
+.home__nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
